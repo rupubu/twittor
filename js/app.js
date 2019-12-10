c944eafc6fe5d9bdfc3db06b8a7505581410b1c9
Rubén Puerta <rupubu@gmail.com>
2019-12-10 13:41:49 +0100

    Modificamos la ruta del SW

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
+let url = window.location.href;
+let swLocation = "/twittor/sw.js";
+
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/sw.js");
+  if (url.includes("localhost")) {
+    swLocation = "/sw.js";
+  }
+  navigator.serviceWorker.register(swLocation);
 }
+
 // Referencias de jQuery
 
 var titulo = $("#titulo");
